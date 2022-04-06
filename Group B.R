f2db2965b9d8506258d01af32422bad065830406
Nick Carnevale <nic225@lehigh.edu>
2022-04-06 11:23:35 -0400

    Project Part 2
    R Project File, .csv file, and R Script

---
+++ Group B.R
@@ -0,0 +1,20 @@
+#Group B
+#Nick C., Sean, John, Nicholas K., Keisha, Rachel
+#Team Project Part 2
+
+#Import .csv file
+DF <- read.csv("food-footprints.csv")
+
+#read .csv file
+DF
+
+#set the code column to null because it contains useless data
+DF$Code <- NULL
+
+#re-read the data frame
+DF
+
+#summarize each column
+summary(DF$Entity)
+summary(DF$Year)
+summary(DF$GHG.emissions.per.kilogram..Poore...Nemecek..2018.)
